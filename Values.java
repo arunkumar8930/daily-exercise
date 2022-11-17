67adc7551397bb361322fb4e298f06ca66ba808f
arunkumar8930 <117913391+arunkumar8930@users.noreply.github.com>
2022-11-17 21:08:33 +0530

    Add files via upload

---
+++ Values.java
@@ -0,0 +1,24 @@
+public class Values
+{
+   public static void main(String[] args)
+   {
+     System.out.println("The Minimum value of Integer is :" +Integer.MIN_VALUE);
+     System.out.println("The Maximum value of Integer is :" +Integer.MAX_VALUE);
+     System.out.println("The Size of Integer is :" +Integer.SIZE);
+     System.out.println("The Minimum value of Byte is:" +Byte.MIN_VALUE);
+     System.out.println("The Maximum value of Byte is :" +Byte.MAX_VALUE);
+     System.out.println("The Size of Byte is :" +Byte.SIZE);
+     System.out.println("The Minimum value of Short is :" +Short.MIN_VALUE);
+     System.out.println("The Maximum value of Short is :" +Short.MAX_VALUE);
+     System.out.println("The Size of Short is :" +Short.SIZE);
+     System.out.println("The Minimum value of Long is :" +Long.MIN_VALUE);
+     System.out.println("The Maximum value of Long is :" +Long.MAX_VALUE);
+     System.out.println("The Size of Long is :" +Long.SIZE);
+     System.out.println("The Minimum value of Float is :" +Float.MIN_VALUE);
+     System.out.println("The Maximum value of Float is :" +Float.MAX_VALUE);
+     System.out.println("The Size of Float is :" +Float.SIZE);
+     System.out.println("The Minimum value of Double is :" +Double.MIN_VALUE);
+     System.out.println("The Maximum value of Double is :" +Double.MAX_VALUE);
+     System.out.println("The Size of Double is :" +Double.SIZE);
+      }
+}
