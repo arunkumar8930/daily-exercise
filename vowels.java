ba40e44a287ddebe2492fa7d5b3e2c20d6d7d191
arunkumar8930 <117913391+arunkumar8930@users.noreply.github.com>
2022-11-23 11:59:14 +0530

    Add files via upload

---
+++ vowels.java
@@ -0,0 +1,11 @@
+public class vowels
+{
+    public static void main(String[] args)
+    {
+        char ch = 'u';
+        if (ch == 'f' || ch == 'g' || ch =='e' || ch == 'i' || ch == 'u')
+        System.out.println(ch + "is vowel");
+        else
+        System.out.println(ch + "is consonent");
+    }
+}
